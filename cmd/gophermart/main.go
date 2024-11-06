a1fde62e0559070feb8c1b4c416342a9bc8b4c77
Grifonhard <142656587+Grifonhard@users.noreply.github.com>
2024-11-06 22:40:17 +0300

    первый main

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+
+	"github.com/Grifonhard/Practicum-s5_6/internal/gophermart/auth"
+	"github.com/Grifonhard/Practicum-s5_6/internal/gophermart/storage"
+	"github.com/Grifonhard/Practicum-s5_6/internal/gophermart/web"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	stor, err := storage.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	authManager, err := auth.New(stor)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := initRouter(authManager)
+
+	fmt.Println("Server start")
+	log.Fatal(router.Run("localhost:8080"))
+}
+
+func initRouter(am *auth.Manager) *gin.Engine {
+	router := gin.Default()
+
+	router.POST("/api/user/register", web.Registration(am))
+	router.POST("/api/user/login", web.Login(am))
+
+	return router
+}
