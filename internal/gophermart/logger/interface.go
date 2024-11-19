39c04d66ee429c029af431492fd636019b0c37aa
Igor <grifonhard@gmail.com>
2024-11-19 13:05:32 +0300

    печать ошибок

--- internal/gophermart/logger/interface.go
+++ internal/gophermart/logger/interface.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"reflect"
 )
 
 var log *Logger
@@ -20,6 +21,18 @@ func Info(msg string, a ...any) {
 }
 
 func Debug(msg string, a ...any) {
+	if len(a) == 1 {
+		arg := a[0]
+		typ := reflect.TypeOf(arg)
+		if typ.Kind() == reflect.Ptr && typ.Elem().Implements(reflect.TypeOf((*error)(nil)).Elem()) {
+			err := arg.(*error) 
+			if err != nil && *err != nil {
+				log.debug(fmt.Sprintf(msg, (*err).Error()))
+				return
+			}
+			return
+		}
+	}
 	log.debug(fmt.Sprintf(msg, a...))
 }
 
